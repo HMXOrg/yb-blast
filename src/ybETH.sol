3b7402cbe2905b9d4a7ca214ec12251e5d7502b6
shutsuwei88 <shu.tsu.wei88@gmail.com>
2024-01-25 10:11:44 +0700

    chore: add comments

--- src/ybETH.sol
+++ src/ybETH.sol
@@ -46,11 +46,14 @@ contract ybETH is ERC20 {
     return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
   }
 
-  /// @notice Claim all pending yield from the wethRebasing contract and update totalAssets.
+  /// @notice Claim all pending yield and update _totalAssets.
   function claimAllYield() public {
     _totalAssets += blast.claimAllYield(address(this), address(this));
   }
 
+  /// @notice Deposit ETH to mint ybETH.
+  /// @dev This is an extension of ERC-4626 function to support native asset deposit.
+  /// @param _receiver The receiver of ybETH.
   function depositETH(address _receiver) public payable returns (uint256 _shares) {
     // Claim all pending yield
     claimAllYield();
@@ -68,6 +71,10 @@ contract ybETH is ERC20 {
     emit Deposit(msg.sender, _receiver, msg.value, _shares);
   }
 
+  /// @notice Deposit WETH to mint ybETH.
+  /// @dev This function follows ERC-4626 standard.
+  /// @param _assets The amount of WETH to deposit.
+  /// @param _receiver The receiver of ybETH.
   function deposit(uint256 _assets, address _receiver) external returns (uint256 _shares) {
     // Claim all pending yield
     claimAllYield();
@@ -89,6 +96,10 @@ contract ybETH is ERC20 {
     emit Deposit(msg.sender, _receiver, _assets, _shares);
   }
 
+  /// @notice Mint ybETH by specifying the amount of ybETH to mint.
+  /// @dev Only work with WETH as we can't transferFrom ETH.
+  /// @param _shares The amount of ybETH to mint.
+  /// @param _receiver The receiver of ybETH.
   function mint(uint256 _shares, address _receiver) external returns (uint256 _assets) {
     // Claim all pending yield
     claimAllYield();
@@ -109,6 +120,13 @@ contract ybETH is ERC20 {
     emit Deposit(msg.sender, _receiver, _assets, _shares);
   }
 
+  /// @notice The actual implementation of redeem
+  /// @dev if msg.sender is not the owner, the caller must have allowance from the owner.
+  /// @dev if allowance is unlimited, then the allowance is not decreased.
+  /// @param _isEthOut Whether to withdraw ETH or WETH.
+  /// @param _shares The amount of ybETH to redeem.
+  /// @param _receiver The receiver of the assets.
+  /// @param _owner The owner of the ybETH.
   function _redeem(bool _isEthOut, uint256 _shares, address _receiver, address _owner)
     internal
     returns (uint256 _assets)
@@ -145,14 +163,30 @@ contract ybETH is ERC20 {
     emit Withdraw(msg.sender, _receiver, _owner, _assets, _shares);
   }
 
+  /// @notice Redeem ybETH to ETH by specifying the amount of ybETH to redeem.
+  /// @param _shares The amount of ybETH to redeem.
+  /// @param _receiver The receiver of the assets.
+  /// @param _owner The owner of the ybETH.
   function redeemETH(uint256 _shares, address _receiver, address _owner) public returns (uint256 _assets) {
     return _redeem(true, _shares, _receiver, _owner);
   }
 
+  /// @notice Redeem ybETH to WETH by specifying the amount of ybETH to redeem.
+  /// @dev This function follows ERC-4626 standard.
+  /// @param _shares The amount of ybETH to redeem.
+  /// @param _receiver The receiver of the assets.
+  /// @param _owner The owner of the ybETH.
   function redeem(uint256 _shares, address _receiver, address _owner) public returns (uint256 _assets) {
     return _redeem(false, _shares, _receiver, _owner);
   }
 
+  /// @notice The actual implementation of withdraw.
+  /// @dev if msg.sender is not the owner, the caller must have allowance from the owner.
+  /// @dev if allowance is unlimited, then the allowance is not decreased.
+  /// @param _isEthOut Whether to withdraw ETH or WETH.
+  /// @param _assets The amount of assets that user wishes to receive.
+  /// @param _receiver The receiver of the assets.
+  /// @param _owner The owner of the ybETH.
   function _withdraw(bool _isEthOut, uint256 _assets, address _receiver, address _owner)
     public
     returns (uint256 _shares)
@@ -188,62 +222,91 @@ contract ybETH is ERC20 {
     emit Withdraw(msg.sender, _receiver, _owner, _assets, _shares);
   }
 
+  /// @notice Withdraw ETH by specifying the amount of assets that user wishes to receive.
+  /// @param _assets The amount of assets that user wishes to receive.
+  /// @param _receiver The receiver of the assets.
+  /// @param _owner The owner of the ybETH.
   function withdrawETH(uint256 _assets, address _receiver, address _owner) public returns (uint256 _shares) {
     return _withdraw(true, _assets, _receiver, _owner);
   }
 
+  /// @notice Withdraw WETH by specifying the amount of assets that user wishes to receive.
+  /// @dev This function follows ERC-4626 standard.
+  /// @param _assets The amount of assets that user wishes to receive.
+  /// @param _receiver The receiver of the assets.
+  /// @param _owner The owner of the ybETH.
   function withdraw(uint256 _assets, address _receiver, address _owner) public returns (uint256 _shares) {
     return _withdraw(false, _assets, _receiver, _owner);
   }
 
+  /// @notice Return the total assets managed by this contract.
+  /// @dev Unclaimed yield is included.
   function totalAssets() public view returns (uint256) {
     return _totalAssets + blast.readClaimableYield(address(this));
   }
 
+  /// @notice Preview the amount of ybETH to mint by specifying the amount of assets to deposit.
+  /// @param _assets The amount of assets to deposit.
   function previewDeposit(uint256 _assets) public view returns (uint256 _shares) {
     return convertToShares(_assets);
   }
 
+  /// @notice Preview the amount of assets to receive by specifying the amount of ybETH to redeem.
+  /// @param _shares The amount of ybETH to redeem.
   function previewRedeem(uint256 _shares) public view returns (uint256 _assets) {
     return convertToAssets(_shares);
   }
 
+  /// @notice Preview the amount of WETH/ETH needed to mint by specifying the amount of ybETH.
+  /// @param _shares The amount of ybETH to mint.
   function previewMint(uint256 _shares) public view returns (uint256 _assets) {
     // SLOAD
     uint256 _totalSupply = totalSupply;
     return _totalSupply == 0 ? _shares : _shares.mulDivUp(totalAssets(), _totalSupply);
   }
 
+  /// @notice Preview the amount of ybETH needed by specifying the amount of WETH/ETH wishes to receive.
+  /// @param _assets The amount of WETH/ETH wishes to receive.
   function previewWithdraw(uint256 _assets) public view returns (uint256 _shares) {
     // SLOAD
     uint256 _totalSupply = totalSupply;
     return _totalSupply == 0 ? _assets : _assets.mulDivUp(_totalSupply, totalAssets());
   }
 
+  /// @notice Convert the amount of assets to ybETH.
+  /// @param _assets The amount of assets to convert.
   function convertToShares(uint256 _assets) public view returns (uint256 _shares) {
     // SLOAD
     uint256 _totalSupply = totalSupply;
     return _totalSupply == 0 ? _assets : _assets.mulDivDown(_totalSupply, totalAssets());
   }
 
+  /// @notice Convert the amount of ybETH to assets.
+  /// @param _shares The amount of ybETH to convert.
   function convertToAssets(uint256 _shares) public view returns (uint256 _assets) {
     // SLOAD
     uint256 _totalSupply = totalSupply;
     return _totalSupply == 0 ? _shares : _shares.mulDivDown(totalAssets(), _totalSupply);
   }
 
+  /// @notice Return the amount of assets that can be deposited to ybETH.
   function maxDeposit(address) public pure returns (uint256) {
     return type(uint256).max;
   }
 
+  /// @notice Return the amount of ybETH that can be minted.
   function maxMint(address) public pure returns (uint256) {
     return type(uint256).max;
   }
 
+  /// @notice Return the amount of ETH/WETH that can be withdrawn from ybETH.
+  /// @param _owner The owner of the ybETH.
   function maxWithdraw(address _owner) public view returns (uint256) {
     return convertToAssets(balanceOf[_owner]);
   }
 
+  /// @notice Return the amount of ybETH that can be redeemed.
+  /// @param _owner The owner of the ybETH.
   function maxRedeem(address _owner) public view returns (uint256) {
     return balanceOf[_owner];
   }
